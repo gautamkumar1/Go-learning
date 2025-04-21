7e2ff5726466880271f7acb5008f700348c4c994
gautamkumar1 <gautamraaz936@gmail.com>
2025-04-21 13:09:49 +0530

    Learned about simple values

---
+++ 2_Simple_Values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//  Int
+	fmt.Println(1+2);
+	// string
+	fmt.Println("helllo")
+	// float
+	fmt.Println(1.2+2.3);
+	// boolean
+	fmt.Println(true && false)
+}
